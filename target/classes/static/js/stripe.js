7284966cf60b375d5c2964e497b25f9ebbfa3bdf
skonkon <amenochihare.428.ao@gmail.com>
2025-02-27 04:12:05 +0900

    Delete target directory

--- target/classes/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = Stripe('pk_test_51Qwng3KP48iI9eCWDJbhWm8RV5Q2LkpEzIc10hCLaVWUrXF9Zr2bImZUUj3hVbkkvwahpj6aLNZEDHopRnGluRJ800fx9bGijy');
-const paymentButton = document.querySelector('#paymentButton');
-
-paymentButton.addEventListener('click', () => {
- stripe.redirectToCheckout({
-   sessionId: sessionId
- })
-});
